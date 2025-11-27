70593c0a7ec9e3c279ab0b883160f8d6aaaa914e
ShiboshreeRoy <shiboshreeroy169@gmail.com>
2025-11-28 01:45:02 +0600

    Update  ShortUrl  model

--- app/models/short_url.rb
+++ app/models/short_url.rb
@@ -1,3 +1,22 @@
 class ShortUrl < ApplicationRecord
   belongs_to :user
+
+  validates :original_url, presence: true, format: URI::DEFAULT_PARSER.make_regexp(%w[http https])
+  validates :short_code, uniqueness: true, allow_nil: true
+  validates :custom_code, uniqueness: true, allow_blank: true
+
+  before_create :generate_short_code
+  before_create :set_expiration
+
+  def generate_short_code
+    self.short_code ||= SecureRandom.alphanumeric(6)
+  end
+
+  def set_expiration
+    self.expires_at ||= 30.days.from_now
+  end
+
+  def active?
+    expires_at.nil? || expires_at > Time.current
+  end
 end
